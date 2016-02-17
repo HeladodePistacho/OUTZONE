77f47041e00674ba16050d38d3a42fe815a58ad2
Ma7rixone <Ma7rixmail@gmail.com>
2016-02-17 21:30:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(){
 	printf("Hola des de Terrassa, soc l' Eric\n");
 	printf("Hola des de Manresa, soc el Ferran\n");
+	printf("Bona nit benaventurats, soc en Víctor\n");
 	getchar();
 	return 0;
 }
