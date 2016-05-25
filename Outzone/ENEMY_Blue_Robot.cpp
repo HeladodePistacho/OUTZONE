71072011ba1f096ccdbec81ba104530f1f33797a
HeladodePistacho <th_sola@hotmail.com>
2016-05-26 01:24:16 +0200

    Blue robot all animations added

--- Outzone/ENEMY_Blue_Robot.cpp
+++ Outzone/ENEMY_Blue_Robot.cpp
@@ -10,74 +10,74 @@ ENEMY_Blue_Robot::ENEMY_Blue_Robot(int x, int y, MOVEMENT_TYPES type) : Enemy(x,
 {
 
 	//IDLE animations
-	down.PushBack({ 3, 2, 28, 35 });
-	left.PushBack({ 114, 50, 23, 33 });
-	right.PushBack({ 162, 3, 24, 35 });
-	up.PushBack({ 274, 47, 28, 35 });
+	down.PushBack({ 674, 4, 28, 34 });
+	left.PushBack({ 785, 48, 23, 33 });
+	right.PushBack({ 850, 6, 28, 31 });
+	up.PushBack({ 958, 46, 27, 36 });
 
-	midle_down_right.PushBack({ 75, 3, 27, 35 });
-	midle_down_right_bot.PushBack({ 38, 3, 29, 34 });
-	midle_down_right_top.PushBack({ 111, 1, 26, 35 });
+	midle_down_right.PushBack({ 782, 1, 26, 35 });
+	midle_down_right_bot.PushBack({ 713, 3, 29, 33 });
+	midle_down_right_top.PushBack({ 749, 0, 27, 35 });
 
-	midle_down_left.PushBack({ 43, 49, 27, 36 });
-	midle_down_left_bot.PushBack({ 8, 50, 26, 35 });
-	midle_down_left_top.PushBack({ 79, 50, 26, 35 });
+	midle_down_left.PushBack({ 713, 47, 27, 35 });
+	midle_down_left_bot.PushBack({ 675, 43, 27, 34 });
+	midle_down_left_top.PushBack({ 750, 47, 26, 35 });
 
-	midle_up_right.PushBack({ 239, 2, 25, 35 });
-	midle_up_right_bot.PushBack({ 203, 2, 22, 36 });
-	midle_up_right_top.PushBack({ 274, 1, 28, 36 });
+	midle_up_right.PushBack({ 921, 4, 25, 34 });
+	midle_up_right_bot.PushBack({ 888, 4, 22, 35 });
+	midle_up_right_top.PushBack({ 921, 4, 25, 34 });
 
-	midle_up_left.PushBack({ 200, 48, 27, 35 });
-	midle_up_left_bot.PushBack({ 161, 50, 24, 35 });
-	midle_up_left_top.PushBack({ 239, 48, 26, 35 });
+	midle_up_left.PushBack({ 885, 47, 28, 35 });
+	midle_up_left_bot.PushBack({ 885, 47, 28, 35 });
+	midle_up_left_top.PushBack({ 885, 47, 28, 35 });
 
 	//MOVEMENT animations
-	mov_down.PushBack({ 167, 231, 27, 35 });
-	mov_down.PushBack({ 205, 234, 27, 32 });
-	mov_down.PushBack({ 243, 233, 28, 33 });
-	mov_down.PushBack({ 282, 235, 27, 31 });
+	mov_down.PushBack({ 848, 186, 27, 35 });
+	mov_down.PushBack({ 884, 189, 27, 32 });
+	mov_down.PushBack({ 921, 188, 28, 33 });
+	mov_down.PushBack({ 960, 190, 27, 31 });
 	mov_down.speed = 0.15f;
 
-	mov_left.PushBack({ 170, 144, 25, 32 });
-	mov_left.PushBack({ 207, 144, 28, 32 });
-	mov_left.PushBack({ 244, 143, 25, 33 });
-	mov_left.PushBack({ 281, 144, 25, 33 });
+	mov_left.PushBack({ 850, 96, 25, 33 });
+	mov_left.PushBack({ 889, 95, 25, 33 });
+	mov_left.PushBack({ 926, 96, 25, 32 });
+	mov_left.PushBack({ 959, 94, 28, 33 });
 	mov_left.speed = 0.15f;
 
-	mov_right.PushBack({ 164, 97, 28, 32 });
-	mov_right.PushBack({ 203, 95, 25, 32 });
-	mov_right.PushBack({ 240, 96, 27, 32 });
-	mov_right.PushBack({ 280, 97, 25, 33 });
+	mov_right.PushBack({ 850, 145, 28, 31 });
+	mov_right.PushBack({ 888, 145, 25, 31 });
+	mov_right.PushBack({ 923, 144, 27, 32 });
+	mov_right.PushBack({ 959, 143, 25, 33 });
 	mov_right.speed = 0.15f;
 
-	mov_up.PushBack({ 174, 188, 26, 36 });
-	mov_up.PushBack({ 208, 187, 27, 37 });
-	mov_up.PushBack({ 243, 189, 26, 35 });
-	mov_up.PushBack({ 277, 189, 26, 35 });
+	mov_up.PushBack({ 851, 228, 26, 36 });
+	mov_up.PushBack({ 888, 227, 27, 37 });
+	mov_up.PushBack({ 926, 228, 26, 35 });
+	mov_up.PushBack({ 959, 228, 26, 35 });
 	mov_up.speed = 0.15f;
 
-	mov_down_left.PushBack({ 10, 232, 27, 33 });
-	mov_down_left.PushBack({ 48, 232, 27, 34 });
-	mov_down_left.PushBack({ 84, 231, 25, 34 });
-	mov_down_left.PushBack({ 115, 232, 27, 34 });
+	mov_down_left.PushBack({ 675, 229, 27, 33 });
+	mov_down_left.PushBack({ 713, 229, 27, 34 });
+	mov_down_left.PushBack({ 750, 229, 25, 34 });
+	mov_down_left.PushBack({ 782, 229, 27, 34 });
 	mov_down_left.speed = 0.15f;
 
-	mov_down_right.PushBack({ 6, 188, 27, 34 });
-	mov_down_right.PushBack({ 41, 189, 26, 33 });
-	mov_down_right.PushBack({ 78, 189, 27, 33 });
-	mov_down_right.PushBack({ 116, 189, 26, 35 });
+	mov_down_right.PushBack({ 672, 185, 27, 34 });
+	mov_down_right.PushBack({ 707, 186, 26, 33 });
+	mov_down_right.PushBack({ 744, 186, 27, 33 });
+	mov_down_right.PushBack({ 782, 186, 26, 35 });
 	mov_down_right.speed = 0.15f;
 
-	mov_up_left.PushBack({ 6, 144, 26, 35 });
-	mov_up_left.PushBack({ 40, 144, 28, 35 });
-	mov_up_left.PushBack({ 76, 142, 28, 34 });
-	mov_up_left.PushBack({ 115, 142, 27, 33 });
+	mov_up_left.PushBack({ 673, 141, 27, 35 });
+	mov_up_left.PushBack({ 709, 141, 27, 35 });
+	mov_up_left.PushBack({ 746, 144, 28, 32 });
+	mov_up_left.PushBack({ 782, 144, 28, 32 });
 	mov_up_left.speed = 0.15f;
 
-	mov_up_right.PushBack({ 0, 97, 29, 34 });
-	mov_up_right.PushBack({ 41, 97, 26, 34 });
-	mov_up_right.PushBack({ 80, 97, 24, 33 });
-	mov_up_right.PushBack({ 115, 95, 25, 36 });
+	mov_up_right.PushBack({ 675, 91, 25, 36 });
+	mov_up_right.PushBack({ 711, 94, 26, 34 });
+	mov_up_right.PushBack({ 749, 92, 24, 33 });
+	mov_up_right.PushBack({ 784, 94, 25, 36 });
 	mov_up_right.speed = 0.15f;
 
 	original_position = position;
